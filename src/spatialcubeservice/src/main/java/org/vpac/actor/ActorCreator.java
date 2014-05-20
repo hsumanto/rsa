f53b5b594083fa7373e8959559bb2a2be4626803
Alex Fraser <adfries@vpac.org>
2014-05-20 17:31:43 +1000

    Added config file to allow SpatialCubeService to join other Akka clusters.

--- src/spatialcubeservice/src/main/java/org/vpac/actor/ActorCreator.java
+++ src/spatialcubeservice/src/main/java/org/vpac/actor/ActorCreator.java
@@ -15,11 +15,12 @@ public class ActorCreator {
 	private static ActorRef frontend = null;
 	
 	private ActorCreator() {
-		Config config = ConfigFactory.parseString("akka.actor.provider = akka.cluster.ClusterActorRefProvider").
-				withFallback(ConfigFactory.parseString("akka.remote.netty.tcp.port = 0")).
-				withFallback(ConfigFactory.parseString("akka.extensions = [akka.contrib.pattern.ClusterReceptionistExtension]"));
+		Config config = ConfigFactory.load("master").
+				withFallback(ConfigFactory.parseString("master.hostip = \"localhost\"")).
+				withFallback(ConfigFactory.parseString("master.port = 2552"));
 		ActorCreator.system = ActorSystem.create("Workers", config);
-		Address address = new Address("akka.tcp", "Workers", "172.31.66.52", 2552);
+		Address address = new Address("akka.tcp", "Workers",
+				config.getString("master.hostip"), config.getInt("master.port"));
 		Cluster.get(system).join(address);
 		ActorCreator.frontend = system.actorOf(Props.create(Frontend.class), "frontend");
 	}
