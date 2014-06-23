c5d140304483245804c968c318194f565d1764ad
Alex Fraser <adfries@vpac.org>
2014-06-23 13:05:51 +1000

    Added Minimum filter, which finds the smaller of two values.

---
+++ src/rsaquery/filter-src/org/vpac/ndg/query/Minimum.java
@@ -0,0 +1,63 @@
+/*
+ * This file is part of the Raster Storage Archive (RSA).
+ *
+ * The RSA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * The RSA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * the RSA.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2014 VPAC Innovations Pty Ltd
+ * http://vpac-innovations.com.au
+ */
+
+package org.vpac.ndg.query;
+
+import java.io.IOException;
+
+import org.vpac.ndg.query.QueryConfigurationException;
+import org.vpac.ndg.query.filter.CellType;
+import org.vpac.ndg.query.filter.Description;
+import org.vpac.ndg.query.filter.Filter;
+import org.vpac.ndg.query.filter.InheritDimensions;
+import org.vpac.ndg.query.filter.Rank;
+import org.vpac.ndg.query.math.BoxReal;
+import org.vpac.ndg.query.math.Element;
+import org.vpac.ndg.query.math.VectorReal;
+import org.vpac.ndg.query.sampling.Cell;
+import org.vpac.ndg.query.sampling.PixelSource;
+
+@Description(name = "Minimum", description = "Returns the smaller value of its"
+		+ " inputs. Can also be used as an 'and' filter for boolean data.")
+@InheritDimensions(from = "in")
+public class Minimum implements Filter {
+
+	// Input fields.
+	@Rank(promote = true, group = "in")
+	public PixelSource inputA;
+	@Rank(promote = true, group = "in")
+	public PixelSource inputB;
+
+	// Output fields.
+	@CellType("inputA")
+	public Cell output;
+
+	Element<?> min;
+
+	@Override
+	public void initialise(BoxReal bounds) throws QueryConfigurationException {
+		min = inputA.getPrototype().getElement().copy();
+	}
+
+	@Override
+	public void kernel(VectorReal coords) throws IOException {
+		min.minOf(inputA.getPixel(coords), inputB.getPixel(coords));
+		output.set(min);
+	}
+}
