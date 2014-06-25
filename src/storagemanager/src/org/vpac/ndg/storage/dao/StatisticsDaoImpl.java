364f21fb6769fd4dc7d927aa9b5017b6f8bcfddd
Alex Fraser <adfries@vpac.org>
2014-06-25 14:55:24 +1000

    Statistics gathering performance improvements.
    Wrapped the saving of TaskCats and DatasetCats in a transcation.
    On my computer (which has a spinning disk), this reduces the time
    to save query stats from 5 minutes to 5 seconds.
    
    Also made sure all access to categories is non-lazy, and wrapped it
    in transctions too. Hopefully this will improve the reliability of
    the database connection overall.

--- src/storagemanager/src/org/vpac/ndg/storage/dao/StatisticsDaoImpl.java
+++ src/storagemanager/src/org/vpac/ndg/storage/dao/StatisticsDaoImpl.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.vpac.ndg.storage.model.DatasetCats;
 import org.vpac.ndg.storage.model.TaskCats;
 import org.vpac.ndg.storage.model.TaskHist;
@@ -12,20 +14,26 @@ import org.vpac.ndg.storage.util.CustomHibernateDaoSupport;
 
 public class StatisticsDaoImpl extends CustomHibernateDaoSupport implements StatisticsDao {
 
+	@Override
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
 	public void saveHist(TaskHist h) {
 		getHibernateTemplate().save(h);
 	}
 
+	@Override
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
 	public void saveCats(TaskCats c) {
 		getHibernateTemplate().save(c);
 	}
 
 	@Override
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
 	public void saveCats(DatasetCats dc) {
 		getHibernateTemplate().save(dc);
 	}
 
 	@Override
+	@Transactional
 	public List<TaskCats> searchCats(String taskId, String catType) {
 		Session session = getSession();
 		Criteria c = session.createCriteria(TaskCats.class, "tc");
@@ -33,10 +41,15 @@ public class StatisticsDaoImpl extends CustomHibernateDaoSupport implements Stat
 		if (catType != null)
 			c.add(Restrictions.eq("tc.name", catType));
 		List<TaskCats> cats = c.list();
+		// Ensure the objects have been fully fetched before leaving the
+		// transaction.
+		if (cats.size() > 0)
+			cats.get(0);
 		return cats;
 	}
 
 	@Override
+	@Transactional
 	public List<DatasetCats> searchCats(String datasetId, String timeSliceId,
 			String bandId, String catType) {
 		Session session = getSession();
@@ -49,15 +62,24 @@ public class StatisticsDaoImpl extends CustomHibernateDaoSupport implements Stat
 		if (catType != null)
 			c.add(Restrictions.eq("dc.name", catType));
 		List<DatasetCats> cats = c.list();
+		// Ensure the objects have been fully fetched before leaving the
+		// transaction.
+		if (cats.size() > 0)
+			cats.get(0);
 		return cats;
 	}
 
 	@Override
+	@Transactional
 	public List<TaskHist> searchHist(String taskId) {
 		Session session = getSession();
 		Criteria c = session.createCriteria(TaskHist.class, "th");
 		c.add(Restrictions.eq("th.taskId", taskId));
 		List<TaskHist> hist = c.list();
+		// Ensure the objects have been fully fetched before leaving the
+		// transaction.
+		if (hist.size() > 0)
+			hist.get(0);
 		return hist;
 	}
 
