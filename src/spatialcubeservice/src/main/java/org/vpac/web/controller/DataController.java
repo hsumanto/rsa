3e20d8b7449a1f2f3bb39fbfaaba3a97aee50106
Alex Fraser <adfries@vpac.org>
2014-07-15 15:31:35 +1000

    Fixed warning.

--- src/spatialcubeservice/src/main/java/org/vpac/web/controller/DataController.java
+++ src/spatialcubeservice/src/main/java/org/vpac/web/controller/DataController.java
@@ -24,8 +24,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
