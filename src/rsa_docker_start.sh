d0193474239650dc113c3b6ecf3d3a5b18e1471d
Alex Fraser <alex@phatcore.com>
2016-04-27 15:24:36 +1000

    Automatically setting seed IP in config file
    When starting an Akka seed node, the node's IP address will be
    inserted into the config file if the config file doesn't yet
    contain that information. This makes it easier to launch the akka
    system in a development environment. If the file already contains
    the seed IP addresses, nothing happens (for deployment).

--- src/rsa_docker_start.sh
+++ src/rsa_docker_start.sh
@@ -2,18 +2,46 @@
 
 mode=$1
 
+SEED_CONF=/var/src/rsa/src/rsaworkers/src/main/resources/seed.conf
+
+function update_config() {
+    # When starting a seed node, check for a placeholder in the config file for
+    # the IP address. That case would indicate a half-configured RSA. To make
+    # development easier, the seed node's IP address is automatically set here.
+    local seed_ip
+    if ! grep -q '<seed-node-ip>' ${SEED_CONF}; then
+        return
+    fi
+    echo "Setting config"
+    seed_ip=$(hostname --ip-address)
+    sed -i.bak "s,<seed-node-ip>,${seed_ip},g" ${SEED_CONF}
+    trap unset_config INT
+    trap unset_config TERM
+}
+
+function unset_config() {
+    if ! [ -f ${SEED_CONF}.bak ]; then
+        return
+    fi
+    echo "Resetting config"
+    mv ${SEED_CONF}.bak ${SEED_CONF}
+}
+
 case ${mode} in
     "web")
         cp -f /var/src/rsa/config/* \
             /var/lib/tomcat${TOMCAT_VERSION}/webapps/rsa/WEB-INF/classes/
         exec /usr/share/tomcat${TOMCAT_VERSION}/bin/catalina.sh run
         ;;
+
     "worker")
         exec /var/src/rsa/src/rsaworkers/build/install/rsaworkers/bin/rsaworkers
         ;;
 
     "seed")
-        exec /var/src/rsa/src/rsaworkers/build/install/rsaworkers/bin/rsaworkers seed
+        update_config
+        /var/src/rsa/src/rsaworkers/build/install/rsaworkers/bin/rsaworkers seed
+        unset_config
         ;;
 
     *)
